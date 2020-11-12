69d72c744ea3271d293c2b9ab8fe5f2e9e2143da
Huseyin <huseyin.demir.harmony@gmail.com>
2020-11-12 14:30:48 -0600

    added Login.feature, fixed CucumberRunner.java, fixed configuration.properties

--- src/test/java/com/nextbasecrm/pages/BasePage.java
+++ src/test/java/com/nextbasecrm/pages/BasePage.java
@@ -1,4 +1,21 @@
 package com.nextbasecrm.pages;
 
+import com.nextbasecrm.utils.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class BasePage {
+
+    public BasePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "pagetitle")
+    private WebElement pageTitle;
+
+    public String getPageTitleText(){
+        return pageTitle.getText().trim();
+    }
+
 }
---
+++ src/test/resources/features/Login.feature
@@ -0,0 +1,10 @@
+# new feature
+# Tags: optional
+
+Feature: Login feature
+
+  @parametrized_login
+  Scenario: Login as help desk
+    Given user is on the login page
+    When user logs in as "help desk"
+    Then user should see Activity Stream
