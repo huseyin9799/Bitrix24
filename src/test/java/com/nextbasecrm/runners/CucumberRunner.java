f03ae75b9b9cd7bfc050e1a48e269cbc71111d4e
Almatkost <almatkost@gmail.com>
2020-11-12 22:13:08 -0600

    1 commit

--- src/test/java/com/nextbasecrm/runners/CucumberRunner.java
+++ src/test/java/com/nextbasecrm/runners/CucumberRunner.java
@@ -13,8 +13,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/nextbasecrm/step_definitions",
-        dryRun = false,
-        tags = "@parametrized_login",
+        dryRun = true,
+        tags = "@poll",
         publish = true
 )
 public class CucumberRunner {
